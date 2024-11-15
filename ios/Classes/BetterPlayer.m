46f3418fed81d299a60c454f53b7b47f2be60323
namnh-0652 <nguyen.huu.nam@sun-asterisk.com>
2024-11-15 15:04:02 +0700

    chore: invisible ios PIP placeholder

--- ios/Classes/BetterPlayer.m
+++ ios/Classes/BetterPlayer.m
@@ -637,6 +637,8 @@ static inline CGFloat radiansToDegrees(CGFloat radians) {
         self._playerLayer.frame = frame;
         self._playerLayer.needsDisplayOnBoundsChange = YES;
         //  [self._playerLayer addObserver:self forKeyPath:readyForDisplayKeyPath options:NSKeyValueObservingOptionNew context:nil];
+        // invisible ios PIP placeholder
+        self._playerLayer.opacity = .0001;
         [vc.view.layer addSublayer:self._playerLayer];
         vc.view.layer.needsDisplayOnBoundsChange = YES;
         if (@available(iOS 9.0, *)) {
