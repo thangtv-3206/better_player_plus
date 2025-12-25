55b2d5da255d276a7317a7799fba3cdc0240d49b
hungnd-3027 <ngo.duc.hung@sun-asterisk,com>
2025-12-25 15:35:00 +0700

    Update missing code

--- lib/src/core/better_player_android.dart
+++ lib/src/core/better_player_android.dart
@@ -24,14 +24,13 @@ class _BetterPlayerAndroidState extends BetterPlayerBaseState<BetterPlayerAndroi
         if (!controller.isPlayerVisible || controller.isPipMode() == true) {
           return;
         }
-        final isDeviceEnableRotate = (await VideoPlayerPlatform.instance.isAutoRotateEnabled() ?? false);
-        final isLandscape = [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight].contains(deviceOrientation);
 
         if (!isFullScreenByRotate &&
             controller.controlsEnabled &&
             !controller.isFullScreen &&
-            isLandscape &&
-            isDeviceEnableRotate) {
+            [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight].contains(deviceOrientation) &&
+            (await VideoPlayerPlatform.instance.isAutoRotateEnabled() ?? false)) {
+          lastLandscapeOrientation = deviceOrientation;
           isFullScreenByRotate = true;
           controller.enterFullScreen();
         } else if (isFullScreenByRotate &&
@@ -40,9 +39,6 @@ class _BetterPlayerAndroidState extends BetterPlayerBaseState<BetterPlayerAndroi
           lastLandscapeOrientation = null;
           SystemChrome.setPreferredOrientations(betterPlayerConfiguration.deviceOrientationsAfterFullScreen);
           controller.exitFullScreen();
-        } else if (isDeviceEnableRotate && isLandscape && controller.isFullScreen && isFullScreenByRotate && controller.controlsEnabled) {
-          // Handle the case where the device is rotated 360 degrees without triggering the 'Enter Fullscreen' toggle
-          SystemChrome.setPreferredOrientations([deviceOrientation]);
         }
       });
     }
